d87dba12fb38c1d4905ffb96d9b1d22c139339db
DLamarG <demondg8@gmail.com>
2024-09-14 14:07:59 -0500

    added algo logic

---
+++ pigLatinTranslator.js
@@ -0,0 +1,11 @@
+var translate = function(word) {
+    if(word.length <2){
+      return word
+    }
+    if(word.length >= 2 && /^[aeiou]/gi.test(word[0])){
+      return word + 'ay'
+    }
+    if(word.length >= 2 && /[^aeiou]/gi.test(word[0])){
+      return word.substring(1,) + word[0]+'ay'
+    }   
+  }
